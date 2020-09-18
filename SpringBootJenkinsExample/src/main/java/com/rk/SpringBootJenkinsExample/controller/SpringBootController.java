eb12e03eb33fbe57e9f4d983188ffcdf860f0ade
pin2man2 <rk1991.tech@gmail.com>
2020-09-18 13:12:22 +0530

    commit

--- SpringBootJenkinsExample/src/main/java/com/rk/SpringBootJenkinsExample/controller/SpringBootController.java
+++ SpringBootJenkinsExample/src/main/java/com/rk/SpringBootJenkinsExample/controller/SpringBootController.java
@@ -1,4 +1,4 @@
-package com.rk.SpringBootJenkinsExample;
+package com.rk.SpringBootJenkinsExample.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
