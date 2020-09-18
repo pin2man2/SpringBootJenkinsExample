7a5eb0b7b16809247ef768e5e2dae9e99666fdcc
pin2man2 <rk1991.tech@gmail.com>
2020-09-18 13:18:47 +0530

    changes controller

--- SpringBootJenkinsExample/src/main/java/com/rk/SpringBootJenkinsExample/controller/SpringBootController.java
+++ SpringBootJenkinsExample/src/main/java/com/rk/SpringBootJenkinsExample/controller/SpringBootController.java
@@ -1,14 +1,15 @@
 package com.rk.SpringBootJenkinsExample.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class SpringBootController {
 	
 	@GetMapping("/home")
 	public String welcome() {
-		
 		return "Welcome to the Devops World!!";
 	}
 
