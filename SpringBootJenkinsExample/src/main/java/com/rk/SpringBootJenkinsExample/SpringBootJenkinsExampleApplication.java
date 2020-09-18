10b87f0850769c838c02502f24a234a9a3b1aa16
pin2man2 <rk1991.tech@gmail.com>
2020-09-18 18:06:34 +0530

    changes for deploye

--- SpringBootJenkinsExample/src/main/java/com/rk/SpringBootJenkinsExample/SpringBootJenkinsExampleApplication.java
+++ SpringBootJenkinsExample/src/main/java/com/rk/SpringBootJenkinsExample/SpringBootJenkinsExampleApplication.java
@@ -2,12 +2,26 @@ package com.rk.SpringBootJenkinsExample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class SpringBootJenkinsExampleApplication {
+@RestController
+public class SpringBootJenkinsExampleApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJenkinsExampleApplication.class, args);
 	}
+	  @Override
+	   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	      return application.sources(SpringBootJenkinsExampleApplication.class);
+	   }
+	  @RequestMapping(value = "/",method = RequestMethod.GET)
+	   public String hello() {
+	      return "Hello World from Tomcat";
+	   }
 
 }
